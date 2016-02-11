1c630f613c34e0ff06ad2f5c00af3d98288d5b4f
Alexey Ruzin <ruzin@kokoc.com>
2016-02-11 23:15:41 +0300

    clean up directory

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,12 @@ from fabric.api import local, task, quiet
 @task
 def release():
     with quiet():
-        version_row = local("grep 'VERSION = ' setup.py", capture=True)
-        version = version_row.split(' = ')[1].strip()
-        version = version[1:-1].split('.')
+        version = local("cat version", capture=True).strip().split('.')
         version[-1] = str(int(version[-1]) + 1)
         new_version = '.'.join(version)
-        local("sed -isetup.py 's/VERSION =.*/VERSION = \"{}\"/g' setup.py".format(new_version))
+        local("echo {} > version".format(new_version))
         local('git commit -am "new version {}"'.format(new_version))
-        local('git tag -a v{0} -m \'new version {0}\''.format(new_version))
+        local('git tag -a v{0} -m \'Release version {0}\''.format(new_version))
         local('git push origin master --tags')
-    local("python setup.py register")
+    local("python setup.py register -r pypi")
     local("python setup.py sdist upload -r pypi")
