58e22c8af30016c0aa01e6b5748e5a1b72b22a14
Alexey Ruzin <ruzin@kokoc.com>
2016-02-11 22:58:12 +0300

    preparing for deploy to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
-
-VERSION = "0.0.1"
+try:
+	with open('version') as vf:
+		VERSION = vf.read().strip()
+except (OSError, IOError) as e:
+	VERSION = '0.1'
 
 setup(
     name='elastic-dict',
-    description="Subclass of dict() for preparing large nested structures",
+    packages=['elastic-dict'],
     version=VERSION,
+    description="Subclass of dict() for preparing large nested structures",
     url='https://github.com/KokocGroup/elastic-dict',
     download_url='https://github.com/KokocGroup/elastic-dict/tarball/v{0}'.format(VERSION),
-    packages=['elastic-dict'],
+    keywords=['dict', 'dictionary', 'django', 'template'],
     install_requires=[],
 )
